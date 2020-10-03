c76d06b6266440cdbfd8cb2c5f22b2b55ce95511
MOUAFFO <63717390+MOUAFFO@users.noreply.github.com>
2020-10-03 02:34:51 +0200

    fichier de connection a la base de donnée avec PDO , NB: le nom de la bas est variable

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+
+    $servname = 'localhost';
+    $dbname = 'base';
+    $user = 'root';
+    $pass = '';
+
+      try {
+
+        $conn = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo " connecte a la base <br>";
+
+      }
+      catch (Exception $e) {
+
+        echo " erreur lors de la connection a la BD";
+
+      }
+
+
+ ?>
